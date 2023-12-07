9c31c6ee5a86d2afe034baee4f7e8d9a213c7e2e
Jyotimsr <122531533+Jyotimsr@users.noreply.github.com>
2023-12-07 16:21:33 +0530

    Update number .py

--- number .py
+++ number .py
@@ -3,9 +3,9 @@ from phonenumbers import timezone,geocoder,carrier
 
 numbers=input("Enter Your No. with+--:")
 phone=phonenumbers.parse(numbers)
-time=timezone.time_zones_for_nimber(phone)
+time=timezone.time_zones_for_number(phone)
 car=carrier.name_for_number(phone,"en")
-reg=geocoder.desription_for_number(phone,"en")
+reg=geocoder.description_for_number(phone,"en")
 print(phone)
 print(time)
 print(car)
